e3488081a3a3dd8d52784f460e0318ea67966532
Dudu-dam <85481237+Dudu-dam@users.noreply.github.com>
2021-06-08 14:03:23 +0200

    cambios en js

---
+++ js/converse.js
@@ -0,0 +1,8 @@
+function cambiarImagen(){
+    var imagn=document.getElementById("imagen");
+    if(imagn.scr=="../img/converseblanca.jpg"){
+        imagn.scr= "../img/converse.jpg";
+    }else{
+        imagn.src="../img/converseblanca.jpg";
+    }
+}
